ff756803a9f965f7dd6d5ecabb675ef3553c8fee
Joki <jokisaputrae30@gmail.com>
2022-03-11 16:34:17 +0700

    underscore

---
+++ src/main/kotlin/5_Underscore.kt
@@ -0,0 +1,14 @@
+fun main() {
+    //deklarasi dan inisialisasi variable-variable
+    val oneMillion = 1_000_000
+    val creditCardNumber = 1234_5678_9012_3456L
+    val socialSecurityNumber = 999_99_9999L
+    val hexBytes = 0xFF_EC_DE_5E
+    val bytes = 0b11010010_01101001_10010100_10010010
+    //tampilan variable menggunakan string template
+    println("oneMillion = $oneMillion")
+    println("creditCardNumber = $creditCardNumber")
+    println("socialSecurityNumber = $socialSecurityNumber")
+    println("hexBytes = $hexBytes")
+    println("bytes = $bytes")
+}
